db0b26c492be79c1772575e2478dba1bb9e72ded
chaeyoung moon <cymoon05@naver.com>
2022-04-18 15:01:19 +0900

    플레이어, 몬스터 구조체 구현 & 직업 선택

--- main.cpp
+++ main.cpp
@@ -19,10 +19,76 @@ enum MAP_TYPE {
 	MM_BACK
 };
 
+enum JOB {
+	JOB_NONE,
+	JOB_KNIGHT,
+	JOB_ARCHER,
+	JOB_WIZARD,
+	JOB_END
+};
+
+#define NAME_SIZE	32
+
+typedef struct {
+	char	strName[NAME_SIZE];
+	char	strJobName[NAME_SIZE];
+	JOB		eJob;
+	int iAttackMin;
+	int iAttackMax;
+	int iArmorMin;
+	int iArmorMax;
+	int iHP;
+	int iHPMax;
+	int iMP;
+	int iMPMax;
+	int iExp;
+	int iLevel;
+}tagPlayer;
+
+typedef struct {
+	char	name[NAME_SIZE];
+	int iAttackMin;
+	int iAttackMax;
+	int iArmorMin;
+	int iArmorMax;
+	int iHP;
+	int iHPMax;
+	int iMP;
+	int iMPMax;
+	int iLevel;
+	int iExp;
+	int iGoldMin;
+	int iGoldMax;
+}tagMonster;
+
 int main() {
+	srand(time(NULL));
+
+	// 시작할 때 플레이어 정보 설정
+	// 플레이어 이름을 입력 받는다.
+	tagPlayer tPlayer = {};
+	cout << "이름 : ";
+	cin.getline(tPlayer.strName, NAME_SIZE - 1);
+
+	int iJob = JOB_NONE;
+
+	while (iJob == JOB_NONE) {
+		system("cls");
+		cout << "1. 기사" << endl;
+		cout << "2. 궁수" << endl;
+		cout << "3. 마법사" << endl;
+		cout << "직업을 선택하세요 : ";
+		cin >> iJob;
+		
+		if (iJob <= JOB_NONE || iJob >= JOB_END)
+			iJob = JOB_NONE;
+
+	}
+
 	// 메인 로비
 	while (true) {
 		system("cls");
+		cout << "******************* 로비 ************************" << endl;
 		cout << "1. 맵" << endl;
 		cout << "2. 상점" << endl;
 		cout << "3. 가방" << endl;
@@ -42,11 +108,12 @@ int main() {
 		case MM_MAP:
 			while (true) {
 				system("cls");
+				cout << "******************* 맵 ************************" << endl;
 				cout << "1. 쉬움" << endl;
 				cout << "2. 보통" << endl;
 				cout << "3. 어려움" << endl;
 				cout << "4. 뒤로 가기" << endl;
-				cout << "맵을 선택하세요 : " << endl;
+				cout << "맵을 선택하세요 : ";
 
 				cin >> iMenu;
 
